5db058f1cbed3907f38474a33b3447b018a478b8
Phoque <lafont_jordan@hotmail.fr>
2021-10-29 10:46:26 +0200

    Update LevelManager.cs
    Basic grid generation with unset Cells

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	float padding;
+	[SerializeField]
+	int rows, columns;
+	[SerializeField]
+	GameObject cellPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	Transform t;
+	GameObject[,] grid;
+
+	private void Awake()
+	{
+		t = GetComponent<Transform>();
+	}
+
+	private void Start()
+	{
+		GenerateGrid();
+	}
+
+	void GenerateGrid()
+	{
+		grid = new GameObject[rows, columns];
+		for (int row = 0; row < rows; row++)
+		{
+			for (int col = 0; col < columns; col++)
+			{
+
+				GameObject cell = Instantiate(cellPrefab, t);
+				cell.transform.position = new Vector2(row * (1 + padding), col * (1 + padding));
+				grid[row, col] = cell;
+			}
+		}
+	}
 }
