c3e78d6be5973c60d2cbea61c93d0ac3e8a2afe2
Phoque <lafont_jordan@hotmail.fr>
2021-10-29 10:59:42 +0200

    Setup camera centering on grid and orthographic size

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -22,6 +23,22 @@ public class LevelManager : MonoBehaviour
 	private void Start()
 	{
 		GenerateGrid();
+		SetCamera();
+	}
+
+	private void SetCamera()
+	{
+		Vector3 cameraPos = grid[rows - 1, columns - 1].transform.position / 2f;
+		float cameraRatio = Camera.main.aspect;
+		Camera.main.transform.position = new Vector3(cameraPos.x, cameraPos.y, -10f);
+		if (cameraPos.x > cameraPos.y * cameraRatio)
+		{
+			Camera.main.orthographicSize = (cameraPos.x + 0.5f + (2 * padding)) / cameraRatio;
+		}
+		else
+		{
+			Camera.main.orthographicSize = cameraPos.y + 0.5f + 2 * padding;
+		}
 	}
 
 	void GenerateGrid()
@@ -34,6 +51,7 @@ public class LevelManager : MonoBehaviour
 
 				GameObject cell = Instantiate(cellPrefab, t);
 				cell.transform.position = new Vector2(row * (1 + padding), col * (1 + padding));
+				cell.name = $"{row}, {col}";
 				grid[row, col] = cell;
 			}
 		}
