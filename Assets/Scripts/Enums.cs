31458cdcecfb77bb7dd748cda6f4feef8ff2c745
Phoque <lafont_jordan@hotmail.fr>
2021-10-29 10:33:33 +0200

    Setup
    added folder structure, enum for cell types, empty cell and levelmanager scripts, empty menu and level scenes

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum CellType
+{
+	Empty,
+	Water,
+	Stone,
+	Seeds,
+	Crops,
+	Zombie,
+	Villager
+}
+
+public class Enums
+{
+}
